c434487e915918a9af6bf5a6393b2a53e031dd8e
AQSA-ABDULLAH <aqsaabdullah38403@gmail.com>
2025-02-12 18:28:02 +0500

    create main section

--- components/main-section/NewsSection.jsx
+++ components/main-section/NewsSection.jsx
@@ -2,39 +2,38 @@ import React from "react";
 
 function NewsSection() {
   return (
-    <div className="p-[8px]">
-      <h2 className="font-bold">MOST READ</h2>
-      <ul className="mt-2 space-y-2">
-        <li>
-          UN says 700,000 children displaced in Gaza amid deadly Israeli attacks
-        </li>
-        <li>
-          Russia and US air strikes attack targets in Syria
-        </li>
-        <li>
-          Israel-Hamas war updates: Palestinians killed in Israeli attack on
-          Jabalia
-        </li>
-        <li>
-          Indonesians boycott McDonald's, Starbucks over support for Israel
-        </li>
-        <li>
-          UK’s Braverman sacked following pro-Palestinian protest comments
-        </li>
-        <li>
-          Anti-Palestinian sentiment rises amid bipartisan US support for Israel
-        </li>
-        <li>
-          What does David Cameron’s return to UK politics mean for the Middle
-          East?
-        </li>
-        <li>
-          Russia-Ukraine war: List of key events, Day 629
-        </li>
-        <li>
-          If Gaza was in your city, how much would be destroyed?
-        </li>
-      </ul>
+    <div className="p-[20px]">
+      <div className="flex items-center text-[20px] gap-[20px]">
+        <img src="/assets/Group 6475.svg" alt="logo" />
+        <h2 className="font-bold">MOST READ</h2>
+      </div>
+      <ul className="mt-[20px] space-y-[20px] text-[14px] tracking-[2.8px]">
+  <li>
+    UN says 700,000 children displaced in Gaza amid deadly Israeli attacks
+  </li>
+  <li>Russia and US air strikes attack targets in Syria</li>
+  <li>
+    Israel-Hamas war updates: Palestinians killed in Israeli attack on
+    Jabalia
+  </li>
+  <li>
+    Indonesians boycott McDonald's, Starbucks over support for Israel
+  </li>
+  <li>
+    UK’s Braverman sacked following pro-Palestinian protest comments
+  </li>
+  <li>
+    Anti-Palestinian sentiment rises amid bipartisan US support for Israel
+  </li>
+  <li>
+    What does David Cameron’s return to UK politics mean for the Middle
+    East?
+  </li>
+  <li>Russia-Ukraine war: List of key events, Day 629</li>
+  <li>If Gaza was in your city, how much would be destroyed?</li>
+</ul>
+
+
     </div>
   );
 }
