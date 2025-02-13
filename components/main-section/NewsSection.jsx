48b8ebe874d454e33dcd112a5f48e7b27039904a
AQSA-ABDULLAH <aqsaabdullah38403@gmail.com>
2025-02-13 13:09:35 +0500

    scroll news

--- components/main-section/NewsSection.jsx
+++ components/main-section/NewsSection.jsx
@@ -1,37 +1,47 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 function NewsSection() {
+  const listRef = useRef(null);
+  const [news, setNews] = useState([
+    "UN says 700,000 children displaced in Gaza amid deadly Israeli attacks",
+    "Russia and US air strikes attack targets in Syria",
+    "Israel-Hamas war updates: Palestinians killed in Israeli attack on Jabalia",
+    "Indonesians boycott McDonald's, Starbucks over support for Israel",
+    "UK’s Braverman sacked following pro-Palestinian protest comments",
+    "Anti-Palestinian sentiment rises amid bipartisan US support for Israel",
+    "What does David Cameron’s return to UK politics mean for the Middle East?",
+    "Russia-Ukraine war: List of key events, Day 629",
+    "If Gaza was in your city, how much would be destroyed?",
+  ]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setNews((prevNews) => [...prevNews.slice(1), prevNews[0]]);
+    }, 3000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <div className="p-[20px]">
+    <div className="p-[20px] overflow-hidden">
       <div className="flex items-center text-[12px] 3xl:text-[20px] gap-[20px] tracking-[2px] 3xl:tracking-[2.8px]">
         <img src="/assets/Group 6475.svg" alt="logo" className="max-3xl:h-8" />
         <h2 className="font-bold">MOST READ</h2>
       </div>
-      <ul className="mt-[12px] 3xl:mt-[22px] space-y-[9px] 3xl:space-y-[18px] text-[10px] 3xl:text-[14px] tracking-[2px] 3xl:tracking-[2.8px] uppercase 3xl:leading-5">
-        <li>
-          UN says 700,000 children displaced in Gaza amid deadly Israeli attacks
-        </li>
-        <li>Russia and US air strikes attack targets in Syria</li>
-        <li>
-          Israel-Hamas war updates: Palestinians killed in Israeli attack on Jabalia
-        </li>
-        <li>
-          Indonesians boycott McDonald's, Starbucks over support for Israel
-        </li>
-        <li>
-          UK’s Braverman sacked following pro-Palestinian protest comments
-        </li>
-        <li>
-          Anti-Palestinian sentiment rises amid bipartisan US support for Israel
-        </li>
-        <li>
-          What does David Cameron’s return to UK politics mean for the Middle East?
-        </li>
-        <li>Russia-Ukraine war: List of key events, Day 629</li>
-        <li>If Gaza was in your city, how much would be destroyed?</li>
-      </ul>
+      <div className="relative overflow-hidden mt-[12px] 3xl:mt-[22px]">
+        <ul
+          ref={listRef}
+          className="space-y-[9px] 3xl:space-y-[18px] text-[10px] 3xl:text-[14px] tracking-[2px] 3xl:tracking-[2.8px] uppercase 3xl:leading-5"
+        >
+          {news.map((item, index) => (
+            <li key={index} className="transition-transform duration-500">
+              {item}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
 
 export default NewsSection;
+
